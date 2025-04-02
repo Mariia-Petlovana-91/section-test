2f7fce272486f6bc19c581582e039c8a7fb4b060
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2025-04-03 01:43:43 +0300

    Updates

--- src/constants/expressions.js
+++
@@ -1,3 +0,0 @@
-export const numberPhone = /^\+380 \d{2} \d{3} \d{4}$/;
-export const email =
-  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
--- src/constants/targetDate.js
+++
@@ -1,3 +0,0 @@
-export const targetDate = new Date(
-  'March 1, 2025 00:00:00',
-).getTime();
