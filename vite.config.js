6b3097d94133e90d59c47a5097b29f0630385e1d
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2025-04-03 01:16:11 +0300

    fix deplou edit config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/section-test/',
+  build: {
+    sourcemap: true,
+  },
 });
